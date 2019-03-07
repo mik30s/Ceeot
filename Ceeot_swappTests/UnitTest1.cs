96d95461f3cf6142abd6eb4efb45325de155421e
Michael Osei <mosei27@live.com>
2019-03-07 15:23:24 -0600

    Add unit tests for createProjects

---
+++ Ceeot_swappTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Ceeot_swapp;
+
+namespace Ceeot_swappTests
+{
+    [TestClass]
+    public class ProjectManagerTest
+    {
+        [TestMethod]
+        public void createProject_All_Empty()
+        {
+            // Arrange
+
+            // Act
+            var projectManager = new ProjectManager();
+            projectManager.createProject("","","","", Project.ProjectVersion.APEX_0604, Project.ProjectVersion.SWATT_2005);
+            // Assert
+            // use mocking framework here.
+        }
+    }
+}
